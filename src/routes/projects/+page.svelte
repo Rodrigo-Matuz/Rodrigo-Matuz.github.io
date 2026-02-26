8b5a95cb21cb8975a91f5953fd68ffc683f1e9fb
Rodrigo-Matuz <rodrigosantoslima1311@gmail.com>
2026-02-26 14:28:54 -0300

    add: projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,19 +1,35 @@
 <script lang="ts">
 	import Header from '$components/header';
+	import ProjectCard from '$components/projectCard';
+	import Space from '$components/space';
+	const title = 'Rodrigo-Matuz • Projects';
 </script>
 
-<div class="flex flex-col bg-background min-h-screen">
-	<Header />
+<svelte:head>
+	<title>{title}</title>
+</svelte:head>
 
-	<div class="flex flex-1 justify-center items-center">
-		<div class="text-center">
-			<h1 class="font-bold text-white text-5xl md:text-7xl tracking-tighter">Work in Progress</h1>
+<Header title="My Projects" />
 
-			<p class="mt-8 text-slate-400 text-2xl">
-				by <span class="font-semibold text-white">MatuZ</span>
-			</p>
+<Space larger={true} />
 
-			<p class="mt-2 font-mono text-slate-500 text-xl">discord • @matuz</p>
-		</div>
+<div class="mx-auto px-5 sm:px-8 lg:px-12 max-w-7xl">
+	<div class="gap-x-6 gap-y-10 md:gap-x-10 lg:gap-x-12 grid grid-cols-1 md:grid-cols-2">
+		<ProjectCard
+			title="Wallpaper Picker UI"
+			description="A simple, lightweight desktop UI for browsing, selecting, and applying wallpapers."
+			github="https://github.com/Rodrigo-Matuz/wallpaper-picker-ui"
+			tags={['Svelte', 'Tailwind', 'TypeScript', 'Rust', 'Linux']}
+			image="/og/WallpaperPickerUIBanner.png"
+			imageAlt="Thumbnail for Wallpaper Picker UI Banner"
+		/>
+		<ProjectCard
+			title="Cinema Automation Challenge - Robot Framework"
+			description="Este repositório foi criado como parte do desafio de automação de testes para uma aplicação de cinema, com o objetivo de praticar e demonstrar conhecimentos avançados em automação de testes web utilizando o Robot Framework com Browser Library."
+			github="https://github.com/Rodrigo-Matuz/cinema-automation-challenge"
+			tags={['Robot Framework', 'Python', 'Playwright', 'QA']}
+		/>
 	</div>
 </div>
+
+<Space larger={true} />
